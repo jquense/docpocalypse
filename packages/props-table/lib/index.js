9d5e609628a07478587826ff3368d51a63e43066
Jason Quense <monastic.panic@gmail.com>
2019-11-27 16:26:49 -0500

    WIP

--- packages/props-table/lib/index.js
+++ packages/props-table/lib/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 exports.default = renderProps;
 
 var _react = _interopRequireDefault(require("react"));
@@ -15,36 +17,30 @@ var _utils = require("./utils");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-
-var isElementType = function isElementType(name, types) {
-  return types.some(function (t) {
+const isElementType = (name, types) => {
+  return types.some(t => {
     if (typeof t === 'string') return t === name;
     return !!name.match(t);
   });
 };
 
-function renderProps(propsData, _temp) {
-  var _ref = _temp === void 0 ? {} : _temp,
-      tokenMap = _ref.tokenMap,
-      _ref$elementTypes = _ref.elementTypes,
-      elementTypes = _ref$elementTypes === void 0 ? ['elementType', /React\.ComponentType(<.*>)?/] : _ref$elementTypes;
-
-  return propsData.filter(function (prop) {
-    return prop.type && !prop.doclets.find(function (d) {
-      return d.tag === 'private' || d.tag === 'ignore';
-    });
-  }).map(function (propData) {
-    var name = propData.name,
-        type = propData.type,
-        defaultValue = propData.defaultValue,
-        description = propData.description,
-        doclets = propData.doclets,
-        tsType = propData.tsType;
-    var docletMap = (0, _utils.docletsToMap)(doclets);
-    var typeName = (0, _utils.getTypeName)(propData);
-    var descHtml = description && description.childMarkdownRemark && description.childMarkdownRemark.html || '';
-    var renderedType = null;
+function renderProps(propsData, {
+  tokenMap,
+  elementTypes = ['elementType', /React\.ComponentType(<.*>)?/]
+} = {}) {
+  return propsData.filter(prop => prop.type && !prop.doclets.find(d => d.tag === 'private' || d.tag === 'ignore')).map(propData => {
+    const {
+      name,
+      type,
+      defaultValue,
+      description,
+      doclets,
+      tsType
+    } = propData;
+    const docletMap = (0, _utils.docletsToMap)(doclets);
+    const typeName = (0, _utils.getTypeName)(propData);
+    const descHtml = description && description.childMarkdownRemark && description.childMarkdownRemark.html || '';
+    let renderedType = null;
     if (tsType) renderedType = _react.default.createElement(_TypescriptTypeValue.default, {
       type: tsType,
       doclets: doclets,
@@ -55,16 +51,16 @@ function renderProps(propsData, _temp) {
       tokens: tokenMap
     });
     return {
-      name: name,
-      doclets: doclets,
-      typeName: typeName,
+      name,
+      doclets,
+      typeName,
       description: descHtml,
       deprecated: docletMap.get('deprecated'),
       type: renderedType,
-      defaultValue: defaultValue && _react.default.createElement(_DefaultValue.default, _extends({}, defaultValue, {
+      defaultValue: defaultValue && _react.default.createElement(_DefaultValue.default, Object.assign({}, defaultValue, {
         isElementType: isElementType(typeName, elementTypes)
       })),
-      propData: propData
+      propData
     };
   });
 } // class PropsTable extends React.Component<PropsTableProps> {
