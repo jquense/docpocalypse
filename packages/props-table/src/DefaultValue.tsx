3589ee79adfb5314a26bb301181118c903207b4c
Jason Quense <monastic.panic@gmail.com>
2019-12-02 10:17:57 -0500

    feat: allow undefined as doclet defaultValue override

--- packages/props-table/src/DefaultValue.tsx
+++ packages/props-table/src/DefaultValue.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 function DefaultValue({ value, isElementType }: Props) {
-  if (value == null) return null;
+  if (value == null || value === 'undefined') return null;
   // eslint-disable-next-line no-param-reassign
   if (isElementType) value = `<${value.replace(/('|")/g, '')}>`;
 
