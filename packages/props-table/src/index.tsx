ea42cf76baec261ed1aeeee89683e3c9e579f452
Jason Quense <monastic.panic@gmail.com>
2019-12-12 16:43:14 -0500

    feat: sort by name

--- packages/props-table/src/index.tsx
+++ packages/props-table/src/index.tsx
@@ -1,3 +1,4 @@
+import sortBy from 'lodash/sortBy';
 import React from 'react';
 import DefaultValue from './DefaultValue';
 import PropTypeValue, { PropType } from './PropTypeValue';
@@ -44,7 +45,7 @@ export default function renderProps(
     elementTypes = ['elementType', /React\.ComponentType(<.*>)?/]
   }: RenderPropsOptions = {}
 ) {
-  return propsData
+  return sortBy(propsData, 'name')
     .filter(
       prop =>
         prop.type &&
@@ -100,93 +101,3 @@ export default function renderProps(
       };
     });
 }
-
-// class PropsTable extends React.Component<PropsTableProps> {
-//   renderRequiredBadge(prop: Prop) {
-//     if (!prop.required) {
-//       return null;
-//     }
-
-//     return <span>required</span>;
-//   }
-
-//   render() {
-//     const { propsData, className } = this.props;
-
-//     const filtered = propsData.filter(
-//       prop =>
-//         prop.type &&
-//         !prop.doclets.find(d => d.tag === 'private' || d.tag === 'ignore'),
-//     );
-
-//     if (!filtered.length) {
-//       return null;
-//     }
-
-//     return (
-//       <table className={`${className || ''} props-table`}>
-//         <thead>
-//           <tr className="props-table-row">
-//             <th className="props-table-header">Name</th>
-//             <th className="props-table-header">Type</th>
-//             <th className="props-table-header">Default</th>
-//             <th className="props-table-header">Description</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {filtered.map(propData => {
-//             const {
-//               name,
-//               type,
-//               defaultValue,
-//               description,
-//               doclets,
-//             } = propData;
-
-//             const descHtml =
-//               (description &&
-//                 description.childMarkdownRemark &&
-//                 description.childMarkdownRemark.html) ||
-//               '';
-
-//             const deprecated = getDoclet(doclets, 'deprecated');
-
-//             return (
-//               <tr key={name} className="props-table-row">
-//                 <td className="props-table-cell  props-table-cell__name">
-//                   {name} {this.renderRequiredBadge(propData)}
-//                 </td>
-//                 <td className="props-table-cell props-table-cell__type">
-//                   {type && <PropTypeValue type={type} doclets={doclets} />}
-//                 </td>
-
-//                 <td className="props-table-cell  props-table-cell__default-value">
-//                   {defaultValue && (
-//                     <DefaultValue
-//                       {...defaultValue}
-//                       isElementType={getTypeName(propData) === 'elementType'}
-//                     />
-//                   )}
-//                 </td>
-
-//                 <td className="props-table-cell">
-//                   {deprecated && (
-//                     <div className="mb-1">
-//                       <strong className="text-danger">
-//                         {`Deprecated: ${deprecated} `}
-//                       </strong>
-//                     </div>
-//                   )}
-
-//                   <div dangerouslySetInnerHTML={{ __html: descHtml }} />
-//                 </td>
-//               </tr>
-//             );
-//           })}
-//         </tbody>
-//       </table>
-//     );
-//   }
-// }
-
-// export default PropsTable;
