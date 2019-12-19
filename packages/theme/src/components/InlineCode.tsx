9e55c987b894bd187e3bedbd78ce804e0db157ba
Jason Quense <monastic.panic@gmail.com>
2019-12-19 16:24:46 -0500

    fix: rogue styled

--- packages/theme/src/components/InlineCode.tsx
+++ packages/theme/src/components/InlineCode.tsx
@@ -1,11 +1,11 @@
-import styled from 'astroturf';
+import dstyled from 'astroturf';
 import React from 'react';
 
 function InlineCode(props: React.HTMLAttributes<HTMLElement>) {
   return <code {...props} />;
 }
 
-export default styled(InlineCode)`
+export default dstyled(InlineCode)`
   @component InlineCode & {
     @theme full minimal {
       @apply text-accent leading-tight px-1 py-1 bg-subtle rounded-sm;
--- test-site/src/@docpocalypse/gatsby-theme/components/InlineCode.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'astroturf';
-import InlineCode from '@docpocalypse/gatsby-theme/src/components/InlineCode';
-
-export default styled(InlineCode)`
-  composes: text-accent leading-tight px-1 py-1 bg-subtle rounded-sm from global;
-`;
